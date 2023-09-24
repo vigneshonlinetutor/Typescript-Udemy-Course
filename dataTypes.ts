b959870c2d414d58697570db2a030df1d9ffe184
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-24 21:39:53 +0200

    Data Types test

---
+++ dataTypes.ts
@@ -0,0 +1,32 @@
+// Boolean
+let isDone: boolean = true;
+let isEnabled: boolean = false;
+
+console.log(isDone);
+console.log(isEnabled);
+
+// Number
+let num1: number = 25;
+let num2: number = 99.99999;
+
+console.log(num1);
+console.log(num2);
+
+// String
+let firstName: string = "Vignesh";
+let lastName: string = 'Playwright';
+
+console.log(firstName);
+console.log(lastName);
+
+// Any
+let dynamicValue: any = 42;
+console.log(dynamicValue);
+dynamicValue = "Vignesh";
+console.log(dynamicValue);
+
+// Null and Undefined
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
+console.log(nullValue);
+console.log(undefinedValue);
