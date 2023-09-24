b703ece91a33ae7ad91aceb463130965d3ce5c67
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-24 21:30:40 +0200

    Added variable test

---
+++ variables.ts
@@ -0,0 +1,10 @@
+var myName = "Vignesh";
+let myAge = 25;
+const myGender = "Male";
+
+console.log("My Name is "+myName+" And my age is "+myAge+ " And I am a "+myGender);
+
+myName = "Playwright";
+myAge = 10;
+
+console.log("My Name is "+myName+" And my age is "+myAge+ " And I am a "+myGender);
