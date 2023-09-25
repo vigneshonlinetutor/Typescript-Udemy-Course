9ecf0a099987f13ca8ed6d5b10fbb85be64b61ce
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-25 17:30:15 +0200

    Tuples Test

---
+++ tuples.ts
@@ -0,0 +1,14 @@
+let person: [string, number] = ['Alice', 25];
+
+let username: string = person[0];
+let userAge: number = person[1];
+
+console.log(username);
+console.log(userAge);
+
+console.log(person);
+
+person[0] = 'Vignesh';
+person[1] = 30;
+
+console.log(person);
